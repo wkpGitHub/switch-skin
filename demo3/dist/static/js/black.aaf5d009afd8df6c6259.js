5d71af6948a3c6124f86b9b44f42c2d006977a89
HZ\houchen <houchen@tsign.cn>
2021-09-08 14:47:46 +0800

    打包生成静态资源，提供访问

---
+++ demo3/dist/static/js/black.aaf5d009afd8df6c6259.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{"1bjM":function(b,n){}},["1bjM"]);
+//# sourceMappingURL=black.aaf5d009afd8df6c6259.js.map
---
+++ demo3/dist/static/js/white.d481b056b33085a6b306.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{Q826:function(n,c){}},["Q826"]);
+//# sourceMappingURL=white.d481b056b33085a6b306.js.map
